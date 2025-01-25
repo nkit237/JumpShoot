294ff5f78b27ec8ba73a75684d764504aa4cfcbb
ev_a <sitfamg@gmail.com>
2025-01-25 15:16:12 +0500

    сбор ящиков (наверное)

--- items.py
+++ items.py
@@ -3,10 +3,16 @@ from settings import *
 from image import *
 
 
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+
 class ItemBox(pygame.sprite.Sprite):
     def __init__(self, item_type, x, y):
         super().__init__()
         self.item_type = item_type
+        self.count = 0
         self.image = item_boxes[self.item_type]
         self.rect = self.image.get_rect()
         self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
@@ -15,15 +21,19 @@ class ItemBox(pygame.sprite.Sprite):
         self.rect.x += SCREEN_SCROLL
         if pygame.sprite.collide_rect(self, player):
             if self.item_type == 'Health':
-                player.health += 25
+                self.count = 25
+                player.health += self.count
                 if player.health > player.max_health:
                     player.health = player.max_health
             elif self.item_type == 'Ammo':
-                player.ammo += 15
+                self.count = 15
+                player.ammo += self.count
             elif self.item_type == 'Grenade':
-                player.grenades += 3
+                self.count = 3
+                player.grenades += self.count
             elif self.item_type == 'Bonus':
                 self.kill()
                 return 10
+            draw_text(f'+{self.count}', pygame.font.SysFont('consolas', 20), 'yellow', self.rect.x, self.rect.y)
             self.kill()
         return 0
